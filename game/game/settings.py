438a955103a18b2b1e44cc965f80317863f85452
dmitry-viskov <dmitry.viskov@webenterprise.ru>
2022-11-30 03:01:49 +0400

    SESSION_COOKIE_SAMESITE / CSRF_COOKIE_SAMESITE settings updates

--- game/game/settings.py
+++ game/game/settings.py
@@ -57,8 +57,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'game.wsgi.application'
 
 SESSION_COOKIE_NAME = 'pylti1p3-django-app-sessionid'
-SESSION_COOKIE_SAMESITE = 'None'
-CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = None
+CSRF_COOKIE_SAMESITE = None
 SESSION_COOKIE_SECURE = False  # should be True in case of HTTPS usage (production)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
